cfa23d9a880dbc3e64b67e75e965cda49d766df1
fayshal69 <foyshalbinamir@gmail.com>
2024-06-01 11:54:49 +0600

    updated navbar

--- src/components/shared/NavBar/NavBar.jsx
+++ src/components/shared/NavBar/NavBar.jsx
@@ -1,18 +1,19 @@
 import { useState, useRef, useEffect } from "react"
 import Container from "../Container/Container"
 import { Link, NavLink } from "react-router-dom"
-import { HiBars3BottomRight } from "react-icons/hi2";
 import { RxCross2 } from "react-icons/rx";
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
 
-// Profile Dropdown
+
 const ProfileDropDown = (props) => {
 
     const [state, setState] = useState(false)
     const profileRef = useRef()
 
     const navigation = [
-        { label: 'Dashboard', to: "/dashboard" },
-        { label: 'Community', to: "/community" }
+        { title: "Profile", path: "/profile" },
+        { title: "Dashboard", path: "/dashboard" },
+        { title: "Community", path: "/community" }
     ]
 
 
@@ -24,72 +25,72 @@ const ProfileDropDown = (props) => {
     }, [])
 
     return (
-        <div className="bg-opacity-80 w-full z-50">
-            <div className={`relative ${props.class} z-50 w-full`}>
-                <div className="">
-                    <div className="flex items-center space-x-4">
-                        <button ref={profileRef} className="w-10 h-10 outline-none rounded-full ring-offset-2 ring-gray-200 ring-2 lg:focus:ring-[#DC5F00]"
-                            onClick={() => setState(!state)}
-                        >
-                            <img
-                                src="https://i.postimg.cc/cJ3DWwSx/360-F-553796090-XHr-E6-R9jwm-BJUMo9-HKl41hy-HJ5gqt9oz.jpg"
-                                className="w-full h-full rounded-full"
-                            />
-                        </button>
-                        <div className="lg:hidden">
-                            <span className="block">Micheal John</span>
-                            <span className="block text-sm">john@gmail.com</span>
-                        </div>
-                    </div>
-                    <ul className={`bg-white rounded p-2 top-12 right-0 mt-5 space-y-5 lg:absolute lg:border lg:rounded-md lg:text-sm lg:w-52 lg:shadow-md lg:space-y-0 lg:mt-0 ${state ? '' : 'lg:hidden'}`}>
-                        {
-                            navigation.map((item, idx) => (
-                                <li key={idx}>
-                                    <Link to={item.to} className="block lg:p-2.5 hover:bg-blue-gray-100 rounded" >{item.label}</Link>
-                                </li>
-                            ))
-                        }
-                        <li className="block lg:p-2.5">
-                            <Link>
-                                <button className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-[#DC5F00] text-[#DC5F00] ">
-                                    <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#DC5F00] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                                    <span className="relative text-[#DC5F00] transition duration-300 group-hover:text-white ease">Login</span>
-                                </button>
-                            </Link>
-                        </li>
-                    </ul>
+        <div className={`relative ${props.class}`}>
+            <div className="flex items-center space-x-4">
+                <button ref={profileRef} className="w-10 h-10 outline-none rounded-full ring-offset-2 ring-gray-200 ring-2 lg:focus:ring-[#DC5F00] ms-3"
+                    onClick={() => setState(!state)}
+                >
+                    <img
+                        src="https://i.postimg.cc/cJ3DWwSx/360-F-553796090-XHr-E6-R9jwm-BJUMo9-HKl41hy-HJ5gqt9oz.jpg"
+                        className="w-full h-full rounded-full z-40"
+                    />
+                </button>
+                <div className="lg:hidden">
+                    <span className="block">Micheal John</span>
+                    <span className="block text-sm text-gray-500">john@gmail.com</span>
                 </div>
             </div>
+            <ul className={`bg-white top-12 right-0 mt-5 space-y-5 lg:absolute lg:border lg:rounded-md lg:text-sm lg:w-52 lg:shadow-md lg:space-y-0 lg:mt-0 ${state ? '' : 'lg:hidden'}`}>
+                {
+                    navigation.map((item, idx) => (
+                        <li key={idx}>
+                            <Link className="block lg:hover:bg-gray-200 lg:p-2.5" to={item.path}>
+                                {item.title}
+                            </Link>
+                        </li>
+                    ))
+                }
+                <li className="block lg:p-2.5">
+                    <Link>
+                        <button className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-[#DC5F00] text-[#DC5F00]">
+                            <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#DC5F00] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                            <span className="relative text-[#DC5F00] transition duration-300 group-hover:text-white ease">Login</span>
+                        </button>
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }
-
 
 const NavBar = () => {
 
     const [menuState, setMenuState] = useState(false)
 
-    const navLinks = [
-        { label: 'Home', to: "/" },
-        { label: 'All Trainer', to: "/all-trainer" },
-        { label: 'All Classes', to: "/all-classes" }
+    const navigation = [
+        { title: "Home", path: "/" },
+        { title: "All Trainer", path: "/all-trainer" },
+        { title: "All Classes", path: "/all-classes" },
     ]
-
     return (
-        <div className="bg-white bg-opacity-80 sticky top-0 z-50">
+        <div className="bg-white bg-opacity-80 sticky top-0 z-30">
             <Container>
-                <nav className="sticky w-full top-0">
-                    <div className="flex justify-between items-center space-x-8 py-3 mx-auto">
+                <nav className=" bg-opacity-80">
+                    <div className="flex items-center space-x-24 py-3">
                         <div className="flex-none lg:flex-initial">
-                            <Link to="/" className=" font-ubuntu font-semibold text-2xl md:text-3xl lg:text-4xl">Fit<span className="text-[#DC5F00]">Vessel</span></Link>
+                            <Link className="text-2xl md:text-3xl lg:text-4xl font-ubuntu font-medium">Fit<span className="text-[#DC5F00]">Vessel</span></Link>
                         </div>
-                        <div className="flex items-center justify-between">
-                            <div className={` absolute z-50 w-full top-16 left-0 p-4 border-b lg:static lg:block lg:border-none ${menuState ? '' : 'hidden'}`}>
-                                <ul className=" flex items-center gap-6 md:gap-8 lg:gap-16">
+                        <div className="flex-1 flex items-center justify-between">
+                            <div className={`bg-white p-3 lg:bg-transparent absolute z-20 w-full top-16 left-0  border-b lg:static lg:block lg:border-none ${menuState ? '' : 'hidden'}`}>
+                                <ul className="mt-12 space-y-5 space-x-5 lg:flex lg:space-x-12 lg:space-y-0 lg:mt-0">
                                     {
-                                        navLinks.map((item, idx) => (
-                                            <li key={idx} className="text-lg font-medium">
-                                                <NavLink to={item.to} className={({isActive}) => isActive? 'text-[#DC5F00] border-b-2 border-[#DC5F00] duration-150' : ''}>{item.label}</NavLink>
+                                        navigation.map((item, idx) => (
+                                            <li key={idx} className="relative inline-flex items-center justify-center leading-normal no-underline text-lg font-medium focus:outline-none focus:ring-2 focus:ring-inset focus:ring-neutral-500 transition group ">
+                                                <NavLink to={item.path} className={({ isActive }) => isActive ? 'text-[#DC5F00]' : 'links'}>{item.title}
+                                                    <span
+                                                        className="absolute bottom-0 left-0 w-full h-[2px] bg-[#DC5F00] bg-opacity-60 origin-bottom-right transform transition duration-200 ease-out scale-x-0 group-hover:scale-x-100 group-hover:origin-bottom-left"
+                                                    ></span>
+                                                </NavLink>
                                             </li>
                                         ))
                                     }
@@ -98,30 +99,29 @@ const NavBar = () => {
                                     class="mt-5 pt-5 border-t lg:hidden"
                                 />
                             </div>
-
-                        </div>
-                        <div className="flex items-center gap-6">
-                            <ProfileDropDown
-                                class="hidden lg:block"
-                            />
-                            <button
-                                className="outline-none text-gray-400 block lg:hidden"
-                                onClick={() => setMenuState(!menuState)}
-                            >
-                                {
-                                    menuState ? (
-                                        <RxCross2 className="text-2xl text-black" />
-                                    ) : (
-                                        <HiBars3BottomRight className="text-2xl text-black" />
-                                    )
-                                }
-                            </button>
+                            <div className="flex-1 flex items-center justify-end space-x-2 sm:space-x-6">
+                                <ProfileDropDown
+                                    class="hidden lg:block"
+                                />
+                                <button
+                                    className="outline-none text-gray-400 block lg:hidden"
+                                    onClick={() => setMenuState(!menuState)}
+                                >
+                                    {
+                                        menuState ? (
+                                            <RxCross2 className="text-2xl text-black" />
+                                        ) : (
+                                            <HiMiniBars3BottomRight className="text-2xl text-black" />
+                                        )
+                                    }
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </nav>
             </Container>
         </div>
-    );
-};
+    )
+}
 
 export default NavBar;
