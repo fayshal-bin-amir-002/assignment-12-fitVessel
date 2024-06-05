a9b5bea53cadc3dbbcde281a5f3567e14df5c5bb
fayshal69 <foyshalbinamir@gmail.com>
2024-06-06 00:55:47 +0600

    updated members dashboard menus

--- src/components/shared/EmptyData/EmptyData.jsx
+++ src/components/shared/EmptyData/EmptyData.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const EmptyData = ({ title }) => {
     return (
-        <div className="min-h-screen w-full flex items-center justify-center">
+        <div className="min-h-[calc(100vh - 64px)] w-full flex items-center justify-center">
             <p className="text-3xl md:text-4xl lg:text-5xl text-gray-700 font-medium">{title}</p>
         </div>
     );
